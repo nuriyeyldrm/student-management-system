6c739f9545369dbf7d95b4e35d0e1c608d25ae86
Nuriye <nry95yldrm@gmail.com>
2022-06-19 17:32:15 +0300

    initial commit

--- src/main/java/dev/proqa/studentmanagementsystem/controller/UserController.java
+++ src/main/java/dev/proqa/studentmanagementsystem/controller/UserController.java
@@ -1,10 +1,15 @@
 package dev.proqa.studentmanagementsystem.controller;
 
 import dev.proqa.studentmanagementsystem.entities.User;
+import dev.proqa.studentmanagementsystem.security.jwt.JwtUtils;
 import dev.proqa.studentmanagementsystem.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +28,8 @@ import java.util.Map;
 public class UserController {
 
     public UserService userService;
+    public AuthenticationManager authenticationManager;
+    public JwtUtils jwtUtils;
 
     @PostMapping("/register")
     public ResponseEntity<Map<String, Boolean>> registerUser(@Valid @RequestBody User user) {
@@ -32,5 +39,24 @@ public class UserController {
         map.put("User registered successfully!", true);
 
         return new ResponseEntity<>(map, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, String>> authenticateUser(@RequestBody Map<String, Object> userMap) {
+        String username = (String) userMap.get("username");
+        String password = (String) userMap.get("password");
+
+        userService.Login(username, password);
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(username, password));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("token", jwt);
+
+        return new ResponseEntity<>(map, HttpStatus.OK);
     }
 }
--- src/main/java/dev/proqa/studentmanagementsystem/service/UserService.java
+++ src/main/java/dev/proqa/studentmanagementsystem/service/UserService.java
@@ -3,14 +3,17 @@ package dev.proqa.studentmanagementsystem.service;
 import dev.proqa.studentmanagementsystem.entities.Role;
 import dev.proqa.studentmanagementsystem.entities.User;
 import dev.proqa.studentmanagementsystem.entities.enumeration.UserRole;
+import dev.proqa.studentmanagementsystem.exception.AuthException;
 import dev.proqa.studentmanagementsystem.exception.ConflictException;
 import dev.proqa.studentmanagementsystem.repository.RoleRepository;
 import dev.proqa.studentmanagementsystem.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.lang.module.ResolutionException;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Service
@@ -38,5 +41,17 @@ public class UserService {
 
         user.setRole(userRole);
         userRepository.save(user);
+    }
+
+    public void Login(String username, String password) throws AuthException {
+        try {
+            Optional<User> user = userRepository.findByUsername(username);
+
+            if (!BCrypt.checkpw(password, user.get().getPassword()))
+                throw new AuthException("Invalid credentials");
+        }
+        catch (Exception e) {
+            throw new AuthException("invalid credentials");
+        }
     }
 }
